1edfd19a426dfba6b98b5358bb536fda3cc8f58e
Mike <35075093+CerteXCm@users.noreply.github.com>
2020-06-17 14:07:54 +0200

    added nullable chain to DataType

--- src/utils/datatype.ts
+++ src/utils/datatype.ts
@@ -1,23 +1,48 @@
 /**
+ * DataType constructor arguments
+ */
+export interface DataTypeOptions {
+  type: string;
+  nullable?: boolean;
+}
+
+/**
  * Class that holds database types mapped to types in typescript
  *
  * TODO:
  *   - Implement the rest of the types
  */
 export class DataType<Type> {
-  public readonly type: string;
-
-  constructor(type: string) {
-    this.type = type;
-  }
-
   public static integer = (): DataType<number> => {
-    return new DataType<number>('integer');
+    return new DataType<number>({ type: 'integer' });
   };
 
   public static string = (): DataType<string> => {
-    return new DataType<string>('string');
+    return new DataType<string>({ type: 'string' });
   };
+
+  /**
+   * Adapter compatible type represented as a string
+   */
+  public readonly type: string;
+
+  /**
+   * Is the type nullable
+   */
+  public isNullable: boolean;
+
+  constructor({ type, nullable = false }: DataTypeOptions) {
+    this.type = type;
+    this.isNullable = nullable;
+  }
+
+  public nullable = (): DataType<Type | undefined> => {
+    this.isNullable = true;
+
+    return this;
+
+    // return new DataType<Type | undefined>({ type: this.type, nullable: true });
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
