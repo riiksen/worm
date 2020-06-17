316794dd393c873bf625d246e916e4ca6502d8bb
ksewo <ksewosteam@gmail.com>
2020-06-17 14:43:36 +0200

    Added Field class

---
+++ src/utils/Field.ts
@@ -0,0 +1,23 @@
+import { DataType } from './Datatypes';
+
+/**
+ * Class that contains a table field definition
+ */
+export class Field<Type> {
+  /**
+   * Name of the column
+   */
+  public readonly name: string;
+
+  /**
+   * Type of the column in database and typescript
+   */
+  public readonly type: DataType<Type>;
+
+  constructor(args: { name: string, type: DataType<Type> }) {
+    this.name = args.name;
+    this.type = args.type;
+  }
+}
+
+export type AnyField = Field<any>;
