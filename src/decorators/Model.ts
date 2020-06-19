835bb95fbf8818a52959a199a2441eafa7e3e0d6
ksewo <ksewosteam@gmail.com>
2020-06-19 14:21:40 +0200

    Implemented Model decorator

--- src/decorators/Model.ts
+++ src/decorators/Model.ts
@@ -1,9 +1,32 @@
+import { BaseModel } from '../model/BaseModel';
+import { withMixins } from '../utils/mixins';
+
 interface ModelConfig {
   tableName: string;
 }
 
-export function Model(config: ModelConfig) {
-  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    
-  }
+type ModelType<M> = { new (): M };
+
+// TODO: add more properties to config
+export function Model(config?: ModelConfig) {
+  return <M>(target: ModelType<M>): void => {
+    const setMixins = withMixins([BaseModel]);
+    setMixins(target);
+
+    const configWithDefaults = {
+      tableName: target.name.toLowerCase(),
+      ...config,
+    } as ModelConfig;
+
+    const propertyDescriptors: PropertyDescriptorMap = {};
+
+    Object.keys(configWithDefaults).forEach((name: keyof ModelConfig) => {
+      propertyDescriptors[name] = {
+        writable: true,
+        value: configWithDefaults[name],
+      };
+    });
+
+    Object.defineProperties(target, propertyDescriptors);
+  };
 }
