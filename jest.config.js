3fe0341eb08adc947c78d77adb3d8f5c3a6ab4c2
ksewo <ksewosteam@gmail.com>
2020-06-19 14:19:00 +0200

    Search for test only in test directory

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,7 @@ const { compilerOptions } = require('./tsconfig.json');
 module.exports = {
   preset: 'ts-jest',
   setupFiles: ['./jest.setup.js'],
+  testEnvironment: 'node',
+  roots: ['./test'],
   // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 };
