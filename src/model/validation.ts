ef8c79bdba38ce45c7b149c366ba10d38981dc28
Mike <35075093+CerteXCm@users.noreply.github.com>
2020-06-22 21:51:14 +0200

    moved the default implementation of validator to Validation mixin

--- src/model/validation.ts
+++ src/model/validation.ts
@@ -1,8 +1,47 @@
-import { container, ValidationResult } from '../container';
+// TODO: Import that only when custom validator function isn't provided in initialize options
+import { validate } from 'class-validator';
+
+import { ModelInstanceInterface } from './instance_interface';
+
 import { Table } from '../utils';
 
+export type ValidationResult<TableT extends Table=Table> = {
+  success: boolean;
+  errors?: {
+    [K in keyof TableT['fields']]?: Record<string, string>;
+  }
+}
+
+export type Validator<M extends ModelInstanceInterface=ModelInstanceInterface> = (
+  (instance: M) => Promise<ValidationResult>
+);
+
+export interface Validation<TableT extends Table=Table> extends ModelInstanceInterface<TableT> {}
+
 export class Validation<TableT extends Table=Table> {
+  static validator: Validator = async <M extends ModelInstanceInterface>(
+    instance: M,
+  ): Promise<ValidationResult> => {
+    const validationErrors = await validate(instance);
+
+    if (!validationErrors.length) {
+      return { success: true };
+    }
+
+    const errors = validationErrors.reduce((acc, error) => {
+      return {
+        ...acc,
+        [error.property]: error.constraints,
+      };
+    }, {} as ValidationResult['errors']);
+
+    return {
+      success: false,
+      errors,
+    };
+  };
+
   public async validate(): Promise<ValidationResult> {
-    return container.validator(this);
+    return Validation.validator(this);
   }
 }
